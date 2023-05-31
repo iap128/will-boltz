a913d8226f96f97598b53fad18cdb393c338c0c4
iap128 <103337777+iap128@users.noreply.github.com>
2023-05-31 12:54:18 -0700

    Updated policy

--- src/components/PolicyList.tsx
+++ src/components/PolicyList.tsx
@@ -5,7 +5,7 @@ const PolicyList = () => {
     'Discounts/refunds are not given for discontinuing a checkride, though I will work with you to reschedule the remaining items.',
     'Completion of a discontinued checkride started with Will is no extra charge.',
     'Completion of a discontinued checkride started with another DPE is charged at the recheck rate.',
-    'Once your checkride has been confirmed, your application in IACRA must be complete and signed no later than 48 hours prior to the start of your checkride, or your checkride will be cancelled.',
+    'Please use IACRA for your checkride applications. There is a $100 fee for using a paper 8710-1 form.',
   ];
 
   return (
@@ -17,6 +17,13 @@ const PolicyList = () => {
           </li>
         );
       })}
+      <li>
+        <Typography.Text strong>
+          Once your checkride has been confirmed, your application in IACRA must be complete and
+          signed no later than 48 hours prior to the start of your checkride, or your checkride will
+          be cancelled.
+        </Typography.Text>
+      </li>
     </ul>
   );
 };
